fef8e126b455341678f76ef8ea465cc6d3bb9dfd
Jordy Ramirez Esquivel <jordystevenramirezesquivel@gmail.com>
2024-03-04 00:59:07 -0600

    adding some code

--- intro.js
+++ intro.js
@@ -1,1 +1,34 @@
 // this is my first code
+monday = 1
+tuesday = 2
+wednesday = 3
+thurdsday = 4
+friday     = 5
+saturday    = 6
+sunday       = 7
+for (let i = 1; i < 8; i++) {
+    
+    day = Math.floor(Math.random()+1)
+    if (i == monday){
+        console.log("monday");
+    }
+    if (i == monday){
+        console.log("tuesday");
+    }
+    if (i == monday){
+        console.log("wednesday");
+    }
+    if (i == monday){
+        console.log("thurdsday");
+    }
+    if (i == monday){
+        console.log("friday");
+    }
+    if (i == monday){
+        console.log("saturday");
+    }
+    if (i == monday){
+        console.log("sunday");
+    }
+
+}
